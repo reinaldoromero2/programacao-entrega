1a9fba7e621a4e8345741f60c26c4213b5deb6d5
reinalripack <61708042-reinalripack@users.noreply.replit.com>
2026-08-06 19:08:48 +0000

    Git commit prior to merge

--- artifacts/programacao-entrega/dev-dist/sw.js
+++ artifacts/programacao-entrega/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.jmq1dv3l4qs"
+    "revision": "0.b5aq968oupc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
